17a38796f266b6df96edde807ab06312e73af7ce
Vasily Levit <vasily.levit@gmail.com>
2022-09-05 11:18:20 +0800

    Work code

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
-print('hello world')
+# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+# Пример:
+# - 6 -> да
+# - 7 -> да
+# - 1 -> нет
+
+day_weak = int(input('Input day of weak: '))
+if day_weak == 6 or day_weak == 7:
+    print(f'{day_weak} -> да')
+else:
+    print(f'{day_weak} -> нет')
